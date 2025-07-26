55cdbd90c7a041de7538dfa7b4deccaaf3f765d3
Rishikesh Badari <rbadari@Rishikeshs-MacBook-Pro.local>
2025-07-26 12:39:18 -0700

    Use custom domain image paths

--- gallery-data.js
+++ gallery-data.js
@@ -1,366 +1,366 @@
 const galleryData = [
     {
-        src: "/75.JPEG",
+        src: "https://www.rishikeshbadari.com/75.JPEG",
         date: "September 6, 2024 at 8:02 PM",
         caption: "backyard at night"
     },
     {
-        src: "/74.JPEG",
+        src: "https://www.rishikeshbadari.com/74.JPEG",
         date: "August 22, 2024 at 9:38 PM",
         caption: "dbr tour"
     },
     {
-        src: "/73.JPEG",
+        src: "https://www.rishikeshbadari.com/73.JPEG",
         date: "August 19, 2024 at 8:43 PM",
         caption: "ali silhouette"
     },
     {
-        src: "/72.JPEG",
+        src: "https://www.rishikeshbadari.com/72.JPEG",
         date: "August 13, 2024 at 12:21 PM",
         caption: "dune buggy"
     },
     {
-        src: "/71.JPEG",
+        src: "https://www.rishikeshbadari.com/71.JPEG",
         date: "August 13, 2024 at 7:45 AM",
         caption: "turtle"
     },
     {
-        src: "/70.JPEG",
+        src: "https://www.rishikeshbadari.com/70.JPEG",
         date: "August 13, 2024 at 7:22 AM",
         caption: "punta cana ocean"
     },
     {
-        src: "/69.JPEG",
+        src: "https://www.rishikeshbadari.com/69.JPEG",
         date: "August 8, 2024 at 2:38 PM",
         caption: "cabs team picture"
     },
     {
-        src: "/68.JPEG",
+        src: "https://www.rishikeshbadari.com/68.JPEG",
         date: "August 6, 2024 at 1:15 PM",
         caption: "panang curry"
     },
     {
-        src: "/67.JPEG",
+        src: "https://www.rishikeshbadari.com/67.JPEG",
         date: "August 2, 2024 at 7:58 PM",
         caption: "sunset"
     },
     {
-        src: "/66.JPEG",
+        src: "https://www.rishikeshbadari.com/66.JPEG",
         date: "August 2, 2024 at 7:55 PM",
         caption: "rainbow outside of BBA gym"
     },
     {
-        src: "/65.JPEG",
+        src: "https://www.rishikeshbadari.com/65.JPEG",
         date: "August 2, 2024 at 6:28 PM",
         caption: "cat"
     },
     {
-        src: "/64.JPEG",
+        src: "https://www.rishikeshbadari.com/64.JPEG",
         date: "August 2, 2024 at 6:28 PM",
         caption: "garfield type cat"
     },
     {
-        src: "/63.JPEG",
+        src: "https://www.rishikeshbadari.com/63.JPEG",
         date: "August 2, 2024 at 6:27 PM",
         caption: "cat outside"
     },
     {
-        src: "/62.JPEG",
+        src: "https://www.rishikeshbadari.com/62.JPEG",
         date: "July 24, 2024 at 9:05 PM",
         caption: "cat at night"
     },
     {
-        src: "/61.JPEG",
+        src: "https://www.rishikeshbadari.com/61.JPEG",
         date: "July 24, 2024 at  PM",
         caption: "cat at night"
     },
     {
-        src: "/60.JPEG",
+        src: "https://www.rishikeshbadari.com/60.JPEG",
         date: "July 21, 2024 at 8:18 PM",
         caption: "cat at night"
     },
     {
-        src: "/59.JPEG",
+        src: "https://www.rishikeshbadari.com/59.JPEG",
         date: "July 21, 2024 at 10:03 AM",
         caption: "cat wouldn't stop staring in the morning"
     },
     {
-        src: "/58.JPEG",
+        src: "https://www.rishikeshbadari.com/58.JPEG",
         date: "July 20, 2024 at 4:47 PM",
         caption: "Oppenheimer Way in Berkeley"
     },
     {
-        src: "/57.JPEG",
+        src: "https://www.rishikeshbadari.com/57.JPEG",
         date: "July 19, 2024 at 8:18 PM",
         caption: "cat after work"
     },
     {
-        src: "/56.JPEG",
+        src: "https://www.rishikeshbadari.com/56.JPEG",
         date: "July 15, 2024 at 7:50 PM",
         caption: "cat after work"
     },
     {
-        src: "/55.JPEG",
+        src: "https://www.rishikeshbadari.com/55.JPEG",
         date: "July 13, 2024 at 7:41 PM",
         caption: "'There Will Be Blood' at Presidio, LA w/ Jatong"
     },
     {
-        src: "/54.JPEG",
+        src: "https://www.rishikeshbadari.com/54.JPEG",
         date: "July 12, 2024 at 8:56 PM",
         caption: "cat on the way to Walmart"
     },
     {
-        src: "/53.JPEG",
+        src: "https://www.rishikeshbadari.com/53.JPEG",
         date: "July 7, 2024 at 7:26 PM",
         caption: "Beverly Hills at night"
     },
     {
-        src: "/52.JPEG",
+        src: "https://www.rishikeshbadari.com/52.JPEG",
         date: "July 7, 2024 at 6:22 PM",
         caption: "dinner at Otus Thai Kitchen & Coffee - Spicy Basil Fried Rice"
     },
     {
-        src: "/51.JPEG",
+        src: "https://www.rishikeshbadari.com/51.JPEG",
         date: "July 7, 2024 at 1:49 PM",
         caption: "'Seven Samurai' 4K Premiere at the Egyptian Theatre"
     },
     {
-        src: "/50.JPEG",
+        src: "https://www.rishikeshbadari.com/50.JPEG",
         date: "July 7, 2024 at 12:46 PM",
         caption: "Michael Jackson's imprint outside the Chinese Theatre"
     },
     {
-        src: "/49.JPEG",
+        src: "https://www.rishikeshbadari.com/49.JPEG",
         date: "July 6, 2024 at 9:01 PM",
         caption: "'TENET' in 70mm at the Egyptian Theatre"
     },
     {
-        src: "/48.JPEG",
+        src: "https://www.rishikeshbadari.com/48.JPEG",
         date: "July 6, 2024 at 5:48 PM",
         caption: "'Inception' in 70mm at the Egyptian Theatre"
     },
     {
-        src: "/47.JPEG",
+        src: "https://www.rishikeshbadari.com/47.JPEG",
         date: "July 6, 2024 at 5:48 PM",
         caption: "ceiling of Egyptian Theatre"
     },
     {
-        src: "/46.JPEG",
+        src: "https://www.rishikeshbadari.com/46.JPEG",
         date: "July 6, 2024 at 5:44 PM",
         caption: "outside the Egyptian Theatre"
     },
     {
-        src: "/45.JPEG",
+        src: "https://www.rishikeshbadari.com/45.JPEG",
         date: "July 6, 2024 at 5:44 PM",
         caption: "Egyptian Theatre"
     },
     {
-        src: "/44.JPEG",
+        src: "https://www.rishikeshbadari.com/44.JPEG",
         date: "July 6, 2024 at 2:13 PM",
         caption: "the 'Ranger' from Interstellar"
     },
     {
-        src: "/43.JPEG",
+        src: "https://www.rishikeshbadari.com/43.JPEG",
         date: "July 6, 2024 at 1:52 PM",
         caption: "restaurant where Michael Corleone kills Sollozo and McClusky"
     },
     {
-        src: "/42.JPEG",
+        src: "https://www.rishikeshbadari.com/42.JPEG",
         date: "July 6, 2024 at 1:52 PM",
         caption: "set pieces from 'The Godfather Pt. 2'"
     },
     {
-        src: "/41.JPEG",
+        src: "https://www.rishikeshbadari.com/41.JPEG",
         date: "July 6, 2024 at 1:32 PM",
         caption: "fake newsletter used in 'The Godfather'"
     },
     {
-        src: "/40.JPEG",
+        src: "https://www.rishikeshbadari.com/40.JPEG",
         date: "July 6, 2024 at 1:25 PM",
         caption: "production pictures from 'The Godfather'"
     },
     {
-        src: "/39.JPEG",
+        src: "https://www.rishikeshbadari.com/39.JPEG",
         date: "July 6, 2024 at 2:56 AM",
         caption: "'Django Unchained' in 35mm at New Beverly Cinema - Tarantino's Theatre!"
     },
     {
-        src: "/38.JPEG",
+        src: "https://www.rishikeshbadari.com/38.JPEG",
         date: "July 5, 2024 at 8:38 PM",
         caption: "Howard Hughes highway in LA at night"
     },
     {
-        src: "/37.JPEG",
+        src: "https://www.rishikeshbadari.com/37.JPEG",
         date: "July 5, 2024 at 3:48 PM",
         caption: "Hollywood Sign as seen from Griffith Park"
     },
     {
-        src: "/36.JPEG",
+        src: "https://www.rishikeshbadari.com/36.JPEG",
         date: "July 5, 2024 at 2:22 PM",
         caption: "original entrance to Warner Bros. Studio"
     },
     {
-        src: "/35.JPEG",
+        src: "https://www.rishikeshbadari.com/35.JPEG",
         date: "July 5, 2024 at 2:08 PM",
         caption: "largest Sound Stage at Warner Bros."
     },
     {
-        src: "/34.JPEG",
+        src: "https://www.rishikeshbadari.com/34.JPEG",
         date: "July 5, 2024 at 1:59 PM",
         caption: "Ingrid Bergman's globe from 'Casablanca'"
     },
     {
-        src: "/33.JPEG",
+        src: "https://www.rishikeshbadari.com/33.JPEG",
         date: "July 5, 2024 at 1:50 PM",
         caption: "Saito's chair from 'Inception'"
     },
     {
-        src: "/32.JPEG",
+        src: "https://www.rishikeshbadari.com/32.JPEG",
         date: "July 5, 2024 at 12:29 PM",
         caption: "information on 'Casablanca'"
     },
     {
-        src: "/31.JPEG",
+        src: "https://www.rishikeshbadari.com/31.JPEG",
         date: "July 5, 2024 at 10:56 AM",
         caption: "Grand Central Market"
     },
     {
-        src: "/30.JPEG",
+        src: "https://www.rishikeshbadari.com/30.JPEG",
         date: "July 4, 2024 at 9:02 PM",
         caption: "'Blowout' at Los Feliz Theatre"
     },
     {
-        src: "/29.JPEG",
+        src: "https://www.rishikeshbadari.com/29.JPEG",
         date: "July 4, 2024 at 2:39 PM",
         caption: "original entrance to Paramount Studios"
     },
     {
-        src: "/28.JPEG",
+        src: "https://www.rishikeshbadari.com/28.JPEG",
         date: "July 4, 2024 at 2:29 PM",
         caption: "original entrance to Paramount Studios"
     },
     {
-        src: "/27.JPEG",
+        src: "https://www.rishikeshbadari.com/27.JPEG",
         date: "July 4, 2024 at 1:52 PM",
         caption: "the Paramount Water Tower"
     },
     {
-        src: "/26.JPEG",
+        src: "https://www.rishikeshbadari.com/26.JPEG",
         date: "July 4, 2024 at 1:24 PM",
         caption: "TARS from 'Interstellar'"
     },
     {
-        src: "/25.JPEG",
+        src: "https://www.rishikeshbadari.com/25.JPEG",
         date: "July 4, 2024 at 10:22 AM",
         caption: "breakfast at Venice Beach"
     },
     {
-        src: "/24.JPEG",
+        src: "https://www.rishikeshbadari.com/24.JPEG",
         date: "June 29, 2024 at 4:40 PM",
         caption: "deer at Yosemite"
     },
     {
-        src: "/23.JPEG",
+        src: "https://www.rishikeshbadari.com/23.JPEG",
         date: "June 29, 2024 at 4:07 PM",
         caption: "view from Yosemite"
     },
     {
-        src: "/22.JPEG",
+        src: "https://www.rishikeshbadari.com/22.JPEG",
         date: "June 22, 2024 at 8:13 PM",
         caption: "Golden Gate Bridge w/ Jatong"
     },
     {
-        src: "/21.JPEG",
+        src: "https://www.rishikeshbadari.com/21.JPEG",
         date: "June 9, 2024 at 3:58 PM",
         caption: "sand castles in Santa Cruz w/ Harshil"
     },
     {
-        src: "/20.JPEG",
+        src: "https://www.rishikeshbadari.com/20.JPEG",
         date: "June 8, 2024 at 3:14 PM",
         caption: "Redwood Trees in Muir Woods"
     },
     {
-        src: "/18.JPEG",
+        src: "https://www.rishikeshbadari.com/18.JPEG",
         date: "May 30, 2024 at 8:33 PM",
         caption: "sunset when biking through Sunnyvale"
     },
     {
-        src: "/16.JPEG",
+        src: "https://www.rishikeshbadari.com/16.JPEG",
         date: "May 20, 2024 at 8:31 AM",
         caption: "cat in Airbnb"
     },
     {
-        src: "/15.JPEG",
+        src: "https://www.rishikeshbadari.com/15.JPEG",
         date: "May 19, 2024 at 6:00 PM",
         caption: "Lakewood Drive in Sunnyvale"
     },
     {
-        src: "/14.JPEG",
+        src: "https://www.rishikeshbadari.com/14.JPEG",
         date: "May 19, 2024 at 5:21 PM",
         caption: "cat in backyard"
     },
     {
-        src: "/13.JPEG",
+        src: "https://www.rishikeshbadari.com/13.JPEG",
         date: "May 5, 2024 at 8:48 PM",
         caption: "Vanderlyn at night"
     },
     {
-        src: "/12.JPEG",
+        src: "https://www.rishikeshbadari.com/12.JPEG",
         date: "April 8, 2024 at 1:20 AM",
         caption: "the Plaza Theatre"
     },
     {
-        src: "/11.JPEG",
+        src: "https://www.rishikeshbadari.com/11.JPEG",
         date: "April 7, 2024 at 9:13 PM",
         caption: "'Cotton Club' w/ Francis Ford Coppola at the Plaza"
     },
     {
-        src: "/10.JPEG",
+        src: "https://www.rishikeshbadari.com/10.JPEG",
         date: "February 1, 2024 at 8:04 PM",
         caption: "'Casino' w/ Naman & Ali at Plaza"
     },
     {
-        src: "/9.JPEG",
+        src: "https://www.rishikeshbadari.com/9.JPEG",
         date: "November 26, 2023 at 6:06 PM",
         caption: "'High and Low' in 35mm at Plaza"
     },
     {
-        src: "/8.JPEG",
+        src: "https://www.rishikeshbadari.com/8.JPEG",
         date: "October 29, 2023 at 7:12 PM",
         caption: "view from the Hub"
     },
     {
-        src: "/7.JPEG",
+        src: "https://www.rishikeshbadari.com/7.JPEG",
         date: "August 14, 2023 at 2:08 PM",
         caption: "'The Harvest of Buckwheat'"
     },
     {
-        src: "/6.JPEG",
+        src: "https://www.rishikeshbadari.com/6.JPEG",
         date: "August 14, 2023 at 1:36 PM",
         caption: "'The Banquet'"
     },
     {
-        src: "/5.JPEG",
+        src: "https://www.rishikeshbadari.com/5.JPEG",
         date: "August 14, 2023 at 1:35 PM",
         caption: "'Midday Sorrow'"
     },
     {
-        src: "/4.JPEG",
+        src: "https://www.rishikeshbadari.com/4.JPEG",
         date: "August 14, 2023 at 1:29 PM",
         caption: "'Chrysanthemums on a Table'"
     },
     {
-        src: "/3.JPEG",
+        src: "https://www.rishikeshbadari.com/3.JPEG",
         date: "August 8, 2023 at 8:26 PM",
         caption: "'JAWS' at Plaza"
     },
     {
-        src: "/2.JPEG",
+        src: "https://www.rishikeshbadari.com/2.JPEG",
         date: "February 26, 2023 at 6:26 PM",
         caption: "view from the hammock Freshman year"
     },
     {
-        src: "/1.JPEG",
+        src: "https://www.rishikeshbadari.com/1.JPEG",
         date: "December 1, 2022 at 3:36 PM",
         caption: "sketch of 'Landscape with Couple Walking and Crescent Moon'"
     }
