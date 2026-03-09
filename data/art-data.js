8f1b3e1d24b4134fe237fdb4305f5a4861cc13fb
Rishikesh Badari <badari.rishikesh@gmail.com>
2026-03-08 18:30:00 -0700

    Add movie links

--- data/art-data.js
+++ data/art-data.js
@@ -28,43 +28,43 @@ var artData = {
     movies: [
         {
             title: "Lawrence of Arabia",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/film/lawrence-of-arabia/"
         },
         {
             title: "Vertigo",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/vertigo/reviews/"
         },
         {
             title: "Malcolm X",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/film/malcolm-x-1992/"
         },
         {
             title: "The Ten Commandments",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/film/the-ten-commandments-1956/"
         },
         {
             title: "Rope",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/rope/"
         },
         {
             title: "Oppenheimer",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/oppenheimer-2023/"
         },
         {
             title: "The Shining",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/the-shining/"
         },
         {
             title: "There Will Be Blood",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/there-will-be-blood/"
         },
         {
             title: "The Graduate",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/the-graduate/"
         },
         {
             title: "High and Low",
-            url: "https://letterboxd.com/rbadari/"
+            url: "https://letterboxd.com/rbadari/film/high-and-low/1/"
         }
     ]
 };
